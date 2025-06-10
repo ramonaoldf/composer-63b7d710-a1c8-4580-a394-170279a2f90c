5df63e53c2c15a097b21d6ebb6d1a9526413454b
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-10 06:15:17 -0300

    Vesion: 001.003.000.000

--- src/ValueObjects/SearchResult.php
+++ src/ValueObjects/SearchResult.php
@@ -24,9 +24,8 @@ class SearchResult implements JsonSerializable
     /**
      * Creates a new search result.
      *
-     * @param Collection $entries
-     * @param string|null $cursor
-     *
+     * @param  Collection  $entries
+     * @param  string|null  $cursor
      * @return void
      */
     public function __construct($entries, $cursor)
@@ -40,6 +39,7 @@ class SearchResult implements JsonSerializable
      *
      * @return array
      */
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return (array) $this;
